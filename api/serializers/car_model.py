08f215844af3604794a2217dd1f254c926c48a22
Alcides Tiago Medeiros Dantas <alcidesdantasdj@gmail.com>
2025-02-10 22:48:51 -0300

    feat: implementa vinculo de car_model com parts.

--- api/serializers/car_model.py
+++ api/serializers/car_model.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import CarModel
+from api.models import CarModel, Part
 
 class CarModelReadSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +10,31 @@ class CarModelWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = CarModel
         fields = ("name", "manufacturer", "year")
+
+class CarModelPartsAssociateSerializer(serializers.ModelSerializer):
+    parts = serializers.PrimaryKeyRelatedField(queryset=Part.objects.all(), many=True)
+
+    class Meta:
+        model = CarModel
+        fields = "__all__"  
+
+    def update(self, instance, validated_data):
+        new_parts = validated_data.get('parts', [])
+        instance.parts.add(*new_parts)
+        return instance
+    
+class CarModelPartsDisassociateSerializer(serializers.ModelSerializer):
+    parts = serializers.PrimaryKeyRelatedField(queryset=Part.objects.all(), many=True)
+
+    class Meta:
+        model = CarModel
+        fields = "__all__"  
+
+    def update(self, instance, validated_data):
+        old_parts = validated_data.pop("parts", [])
+        instance.parts.remove(*old_parts)
+        return instance
+
+class CarModelPartsActionSerializer(serializers.Serializer):
+    operation = serializers.ChoiceField(choices=["associate", "disassociate"], required=True)
+    parts = serializers.PrimaryKeyRelatedField(queryset=Part.objects.all(), many=True, required=True)
--- api/views/car_model.py
+++ api/views/car_model.py
@@ -1,13 +1,18 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 from ..permissions import IsAdministrador, IsUser
 from ..models import CarModel
-from ..serializers import CarModelReadSerializer, CarModelWriteSerializer
+from ..serializers import CarModelReadSerializer, CarModelWriteSerializer, CarModelPartsAssociateSerializer, CarModelPartsDisassociateSerializer, CarModelPartsActionSerializer
 from ..filters import CarModelFilter
 
 class CarModelViewSet(viewsets.ModelViewSet):
     """
-        ViewSet para manipular Car Models
+    ViewSet para manipular Car Models
     """
     queryset = CarModel.objects.all()
     filter_backends = [DjangoFilterBackend]
@@ -19,4 +24,30 @@ class CarModelViewSet(viewsets.ModelViewSet):
         if self.action in ['list', 'retrieve']:
             return CarModelReadSerializer
         return CarModelWriteSerializer
+
+    @extend_schema(
+        request=CarModelPartsActionSerializer, 
+        responses=CarModelReadSerializer
+    )
+    @action(detail=True, methods=['patch'], permission_classes=[IsAdministrador])
+    def parts(self, request, pk):
+        """
+        Endpoint para associar ou desassociar parts a um car model
+        """
+        operation = request.data.get("operation")
+
+        if operation not in ["associate", "disassociate"]:
+            return Response({"error": "Operation must be 'associate' or 'disassociate'."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        car_model = get_object_or_404(CarModel, id=pk)        
+
+        if operation == "associate":
+            serializer = CarModelPartsAssociateSerializer(car_model, data=request.data, partial=True)
+        elif operation == "disassociate":
+            serializer = CarModelPartsDisassociateSerializer(car_model, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
