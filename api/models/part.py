22d5321144b919e3642bde5ff0afe68b603cf925
Alcides Tiago Medeiros Dantas <alcidesdantasdj@gmail.com>
2025-02-10 22:49:16 -0300

    fix: remove validacao com warning.

--- api/models/part.py
+++ api/models/part.py
@@ -5,7 +5,7 @@ class Part(models.Model):
     part_number = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     details = models.TextField(max_length=555)
-    price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(0.01)])
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveIntegerField()
     update_at = models.DateTimeField(auto_now=True)
 
