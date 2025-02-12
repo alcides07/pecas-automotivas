aaa055679f4f897f362e1b780a3e57cfe8095387
Alcides Tiago Medeiros Dantas <alcidesdantasdj@gmail.com>
2025-02-12 20:01:07 -0300

    feat: implementa serializer de resultado de task.

--- api/serializers/tasks.py
+++ api/serializers/tasks.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
+from celery.result import AsyncResult
 
 class SimpleTaskSerializer(serializers.Serializer):
     task_id = serializers.CharField()
     message = serializers.CharField()
 
+class TaskResultSerializer(serializers.Serializer):
+    task_id = serializers.CharField()
+    status = serializers.CharField(required=False)
+    result = serializers.CharField(required=False)
+
+    def validate(self, data):
+        task_id = data.get('task_id')
+        task_result = AsyncResult(task_id)
+
+        data['status'] = task_result.status
+        data['result'] = str(task_result.result)
+        
+        return data
