8341512d5dea091a9538529de0f988dbe7b403dc
Alcides Tiago Medeiros Dantas <alcidesdantasdj@gmail.com>
2025-02-12 22:32:25 -0300

    feat: implementa gestao de estoque.

--- api/serializers/car_model.py
+++ api/serializers/car_model.py
@@ -1,7 +1,12 @@
+from django.db import transaction
 from rest_framework import serializers
-from api.models import CarModel, Part
+from rest_framework.exceptions import ValidationError
+from ..models import CarModel, Part
+from ..serializers import PartReadSerializer
 
 class CarModelReadSerializer(serializers.ModelSerializer):
+    parts = PartReadSerializer(many=True, read_only=True)
+
     class Meta:
         model = CarModel
         fields = "__all__"
@@ -20,9 +25,21 @@ class CarModelPartsAssociateSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         new_parts = validated_data.get('parts', [])
-        instance.parts.add(*new_parts)
-        return instance
-    
+
+        with transaction.atomic():
+            for part in new_parts:
+                if (part in instance.parts.all()):
+                    raise ValidationError(f"{part.name} (ID {part.id}) já está associada a {instance.name} (ID {instance.id}).")
+
+                if (part.quantity <= 0):
+                    raise ValidationError(f"{part.name} (ID {part.id}) está sem estoque.")
+
+                instance.parts.add(part)
+                part.quantity -= 1
+                part.save()
+
+            return instance
+        
 class CarModelPartsDisassociateSerializer(serializers.ModelSerializer):
     parts = serializers.PrimaryKeyRelatedField(queryset=Part.objects.all(), many=True)
 
@@ -32,8 +49,17 @@ class CarModelPartsDisassociateSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         old_parts = validated_data.pop("parts", [])
-        instance.parts.remove(*old_parts)
-        return instance
+
+        with transaction.atomic():
+            for part in old_parts:
+                if (part not in instance.parts.all()):
+                    raise ValidationError(f"NÃO existe {part.name} (ID {part.id}) vinculado(a) a {instance.name} (ID {instance.id}).")
+
+                instance.parts.remove(part)
+                part.quantity += 1
+                part.save()
+
+            return instance
 
 class CarModelPartsActionSerializer(serializers.Serializer):
     operation = serializers.ChoiceField(choices=["associate", "disassociate"], required=True)
