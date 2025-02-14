bce7b9e8f2a196d63ecc7b7caee7ad7f2bbe1752
Alcides Tiago Medeiros Dantas <alcidesdantasdj@gmail.com>
2025-02-14 15:40:56 -0300

    fix: ativa filtro de year em car_model.

--- api/filters/car_model.py
+++ api/filters/car_model.py
@@ -4,7 +4,7 @@ from ..models import CarModel
 class CarModelFilter(filters.FilterSet):
     name = filters.CharFilter(lookup_expr='icontains')
     manufacturer = filters.CharFilter(lookup_expr='icontains')
-    # year = filters.NumberFilter(field_name="year", lookup_expr='exact')
+    year = filters.NumberFilter(field_name="year", lookup_expr='exact')
     year_gte = filters.NumberFilter(field_name="year", lookup_expr='gte')
     year_lte = filters.NumberFilter(field_name="year", lookup_expr='lte')
 
