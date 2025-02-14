396b6542c9f5d2658f898cb287e3f7f06d986bb3
Alcides Tiago Medeiros Dantas <alcidesdantasdj@gmail.com>
2025-02-14 02:19:08 -0300

    fix: corrige related_name.

--- api/models/car_model.py
+++ api/models/car_model.py
@@ -5,7 +5,7 @@ class CarModel(models.Model):
     name = models.CharField(max_length=255)
     manufacturer = models.CharField(max_length=255)
     year = models.PositiveIntegerField()
-    parts = models.ManyToManyField(Part, related_name="parts", blank=True)
+    parts = models.ManyToManyField(Part, related_name="car_models", blank=True)
 
     def __str__(self):
         return f"{self.name} - {self.manufacturer} ({self.year})"
