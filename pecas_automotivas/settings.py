fb139c149b72f05e95b87eb0539afef580d28ef3
Alcides Tiago Medeiros Dantas <alcidesdantasdj@gmail.com>
2025-02-14 15:40:02 -0300

    chore: padroniza paginacao de 100.

--- pecas_automotivas/settings.py
+++ pecas_automotivas/settings.py
@@ -143,6 +143,7 @@ SIMPLE_JWT = {
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
